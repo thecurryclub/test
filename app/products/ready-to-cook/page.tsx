3d866ae9810734ce8dbabf2c002ecc609c934672
thecurryclub <curryclub@vaahfoods.com>
2025-09-20 15:25:45 +0400

    Update page.tsx

--- app/products/ready-to-cook/page.tsx
+++ app/products/ready-to-cook/page.tsx
@@ -3,5 +3,5 @@ import dynamic from "next/dynamic";
 const Collection = dynamic(() => import("@/components/products/Collection"), { ssr: false });
 
 export default function Page(){
-  return <Collection title="Ready-to-Cook" subtitle="Bases, sauces, and marinades to cook fresh." defaultFilters=['"readyToCook"'] />;
+  return <Collection title="Ready-to-Cook" subtitle="Bases, sauces, and marinades to cook fresh." defaultFilters={['"readyToCook"']} />;
 }
