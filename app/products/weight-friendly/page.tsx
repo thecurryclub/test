a877a1617f92c0f7c999cc7b7deb82907c3ce520
thecurryclub <curryclub@vaahfoods.com>
2025-09-20 15:27:46 +0400

    Update page.tsx

--- app/products/weight-friendly/page.tsx
+++ app/products/weight-friendly/page.tsx
@@ -3,5 +3,5 @@ import dynamic from "next/dynamic";
 const Collection = dynamic(() => import("@/components/products/Collection"), { ssr: false });
 
 export default function Page(){
-  return <Collection title="Weight-friendly" subtitle="Lighter sauces and toppings that go big on flavor." defaultFilters=['"weight-friendly"'] />;
+  return <Collection title="Weight-friendly" subtitle="Lighter sauces and toppings that go big on flavor." defaultFilters={['"weight-friendly"']} />;
 }
