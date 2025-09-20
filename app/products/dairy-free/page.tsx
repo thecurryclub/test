42567c203e5d0f9123882c679056ffde8adbf9de
thecurryclub <curryclub@vaahfoods.com>
2025-09-20 15:24:54 +0400

    Update page.tsx

--- app/products/dairy-free/page.tsx
+++ app/products/dairy-free/page.tsx
@@ -3,5 +3,5 @@ import dynamic from "next/dynamic";
 const Collection = dynamic(() => import("@/components/products/Collection"), { ssr: false });
 
 export default function Page(){
-  return <Collection title="Dairy-free" subtitle="Dairy-free options without compromise." defaultFilters=['"dairy-free"'] />;
+  return <Collection title="Dairy-free" subtitle="Dairy-free options without compromise." defaultFilters={['"dairy-free"']} />;
 }
