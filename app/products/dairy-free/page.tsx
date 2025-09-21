18818e02c346fb4e1051250fef9fd20fd43fce95
thecurryclub <curryclub@vaahfoods.com>
2025-09-21 22:16:33 +0400

    Delete app/products/dairy-free directory

--- app/products/dairy-free/page.tsx
+++
@@ -1,7 +0,0 @@
-export const metadata = { title: "Dairy-free" };
-import dynamic from "next/dynamic";
-const Collection = dynamic(() => import("@/components/products/Collection"), { ssr: false });
-
-export default function Page(){
-  return <Collection title="Dairy-free" subtitle="Dairy-free options without compromise." defaultFilters={['"dairy-free"']} />;
-}
